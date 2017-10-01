0911d1a7c0559dedd48a33fbdb1323369bf34804
Nagaraj N <nagaraj.n@cognizant.com>
2017-10-02 00:30:03 +0530

    Initial Commit

---
+++ public/libs/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAObHVczGvXXXKmEkhKgN8gizxZAusQT7U",
+    authDomain: "fill-box.firebaseapp.com",
+    databaseURL: "https://fill-box.firebaseio.com",
+    projectId: "fill-box",
+    storageBucket: "fill-box.appspot.com",
+    messagingSenderId: "756989029696"
+};
+firebase.initializeApp(config);
